1d565fc3b7dd20138d8601bcdf7356a2758c6ace
Edin Hodzic <edin.hodzic@digital.hmrc.gov.uk>
2016-09-27 12:22:41 +0100

    added token exchange

--- kong/plugins/oauth2/access.lua
+++ kong/plugins/oauth2/access.lua
@@ -8,7 +8,9 @@ local responses = require "kong.tools.responses"
 local constants = require "kong.constants"
 local timestamp = require "kong.tools.timestamp"
 local singletons = require "kong.singletons"
-
+local http = require "resty.http"
+local json = require "cjson"
+local util = require "cjson.util"
 local string_find = string.find
 local req_get_headers = ngx.req.get_headers
 local check_https = utils.check_https
@@ -411,24 +413,51 @@ local function parse_access_token(conf)
   return result
 end
 
+local function logDebug(message)
+  ngx.log(ngx.DEBUG, message)
+  print(message) -- TODO remove this line once we figure out how to change the logging level
+end
+
 local function print_table(message, myTable)
-  print("\n\n")
-  print(string.format("------------ [%s] start ------------", message))
+  logDebug("\n\n")
+  logDebug(string.format("------------ [%s] start ------------", message))
   for k, v in pairs( myTable ) do
-    print(string.format("key[%s] value [%s]\t\t", k, v))
+    logDebug(string.format("key[%s] value [%s]\t\t", k, v))
   end
-  print(string.format("------------ [%s] end ------------", message))
-  print("\n\n")
+  logDebug(string.format("------------ [%s] end ------------", message))
+  logDebug("\n\n")
 end
 
 local function validate_scope(token)
   local isValidScope = utils.table_contains(ngx.ctx.plugins_for_request.oauth2.scopes, token.scope)
   if not isValidScope then
-    ngx.log(ngx.DEBUG, string.format("scope validation failed for request path [%s], token [%s %s] and scope [%s]", ngx.ctx.api.request_path, token.token_type, token.access_token, token.scope))
-    -- TODO remove the print(..) once we figure out how to change the logging level
-    print(string.format("scope validation failed for request path [%s], token [%s %s] and scope [%s]", ngx.ctx.api.request_path, token.token_type, token.access_token, token.scope))
+    logDebug(string.format("scope validation failed for request path [%s], token [%s %s] and scope [%s]", ngx.ctx.api.request_path, token.token_type, token.access_token, token.scope))
   end
   return isValidScope
+end
+
+local function exchangeToken(token)
+  local delegatedAuthorityUrl = string.format("http://192.168.224.16:9606/authority?access_token=%s", token.access_token)
+--  local delegatedAuthorityUrl = "http://192.168.224.16:9606/authority?access_token=3d35ec66daf3bf62c09b7b40242b211f"
+  local res, err = http.new():request_uri(delegatedAuthorityUrl, {
+    method = "GET",
+    headers = {
+      ["Content-Type"] = "application/json",
+    }
+  })
+
+  if err then
+    return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
+  elseif res.status == 404 then
+    return responses.send_HTTP_NOT_FOUND(res.body)
+  elseif not res.status == 200 then
+    return responses.send_HTTP_INTERNAL_SERVER_ERROR("some useful message here?")
+  end
+
+  local authBearerToken = json.decode(res.body).delegatedAuthority.user.authBearerToken
+
+  logDebug(string.format("exchanged oauth token [%s] for auth token [%s]", token.access_token, authBearerToken))
+  ngx.req.set_header("Authorization", "Bearer "..authBearerToken)
 end
 
 function _M.execute(conf)
@@ -463,6 +492,8 @@ function _M.execute(conf)
       return responses.send_HTTP_UNAUTHORIZED({[ERROR] = "invalid_token", error_description = "The access token is invalid or has expired"}, {["WWW-Authenticate"] = 'Bearer realm="service" error="invalid_token" error_description="The access token is invalid or has expired"'})
     end
   end
+
+  exchangeToken(token)
 
   if not validate_scope(token) then
     return responses.send_HTTP_UNAUTHORIZED({[ERROR] = "invalid_request", error_description = "Scope validation failed"}, {["WWW-Authenticate"] = 'Bearer realm="service" error="invalid_request" error_description="Scope validation failed"'})
